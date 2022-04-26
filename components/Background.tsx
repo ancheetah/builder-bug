4a53c7b03111519d2a47cb51f7311b1611fc54e9
AJ <ancheetah@users.noreply.github.com>
2022-04-26 15:49:31 -0400

    add input to custom component

--- components/Background.tsx
+++ components/Background.tsx
@@ -1,11 +1,23 @@
 import { Builder } from "@builder.io/react";
 
-export const Background = () => {
-  return <div className="background-color">Sample Text</div>;
+export const Background = (props) => {
+  return <div 
+    className="background-color" 
+    style={{'backgroundColor': `${props.backgroundColor}`}}
+    >
+      Sample Text
+    </div>;
 };
 
 export async function componentRegister() {
   Builder.registerComponent(Background, {
     name: "Background",
+    inputs: [
+      {
+        name: "backgroundColor",
+        type: "color",
+        defaultValue: "#6046e5"
+      }
+    ]
   });
 }
